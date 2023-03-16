7a252754b2a73f66404914ccfe5ad32dbbc16d76
cies <ciesi-AT@stager-DOT.nl>
2023-03-16 17:36:25 +0100

    Make version variable

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-printf "Kotlin compiler version: " && grep kotlin.multiplatform build.gradle | cut -d"'" -f4
-printf "kotlinx.html version: " && grep kotlinx-html build.gradle | cut -d: -f3 | cut -d'"' -f1
+if [[ ! $# -eq 2 ]]; then
+  printf "Fatal error: Two arguments are required. The Kotlin version and the kotlinx.html version.\nExample: $0 1.8.0 0.8.0"
+  exit 1
+fi
 
-echo "run, realCPU, userCPU, peakMEM"
+printf "Kotlin compiler version: $1\n" 
+printf "kotlinx.html version: $2\n"
+
+cp build.gradle.template build.gradle
+sed -i "s/KOTLIN_VERSION/$1/" build.gradle
+sed -i "s/KOTLINXHTML_VERSION/$2/" build.gradle
+
+printf "\nrun, realCPU, userCPU, peakMEM\n"
 for i in {0..8}; do
   ./gradlew clean --console=plain --quiet || break;
   /usr/bin/time -f "$i, %E, %U, %M" ./gradlew compileKotlinJvm --console=plain --quiet || break;
